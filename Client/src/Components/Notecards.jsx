41f10fb93d256bd3d16244b3435963b8b6c76cac
Gautams21 <105819513+Gautams21@users.noreply.github.com>
2024-10-18 21:51:44 +0530

    Update Notecards.jsx

--- Client/src/Components/Notecards.jsx
+++ Client/src/Components/Notecards.jsx
@@ -3,8 +3,8 @@ import { MdCreate, MdDelete, MdOutlinePushPin } from 'react-icons/md'
 
 function Notecards({ title, date, content, tags, isPinned, isEdit, isDeleted, onPinnote }) {
   return (
-    <div className='w-[100%]'>
-      <div className=' border border-gray-400 p-3 rounded-lg'>
+    <div className='w-[100%] '>
+      <div className=' border border-gray-400 p-3 rounded-lg h-full flex flex-col'>
         <div className='flex justify-between'>
           <div>
             <h5 className='text-gray-900 font-semibold'>{title}</h5>
@@ -14,8 +14,8 @@ function Notecards({ title, date, content, tags, isPinned, isEdit, isDeleted, on
             className={`icon-btn ${isPinned ? 'text-blue-400' : 'text-slate-400'} cursor-pointer`}
             onClick={onPinnote}
           />
-        </div>
-        <p className='text-wrap break-words'>
+        </div> 
+        <p className='flex-grow text-wrap break-words'>
           {content.slice(0,60)+'...'}
         </p>
         <div className='flex justify-between'>
